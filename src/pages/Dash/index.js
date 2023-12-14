d524ec0b912349fa8b5c0748fd72f6729840f345
Lucas Alves <Lucas Alves@MG-TI-001N>
2023-12-14 13:54:38 -0300

    add chart

--- src/pages/Dash/index.js
+++ src/pages/Dash/index.js
@@ -3,33 +3,66 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import ChartPizza from '../components/chartPizza'
 
 export default function Dash() {
     const navigation = useNavigation();
     const [posts, setposts] = useState([]);
-
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://192.168.9.50/Json/Teste.json');
+                const response = await axios.get('http://192.168.9.50/Json/desvios.json');
                 setposts(response.data);
-                //console.log('Resposta da API:',response.data);
             } catch (error) {
-                console.Log('Erro ao buscar dados da API:', error);
+                console.log("Erro ao buscar dados na api");
             }
         };
+        //console.log(posts);
         fetchData();
     }, []);
-    console.log(posts)
     return (
         <View style={style.container}>
-            <Text>Dash</Text>
-            <Text>Cargo: {posts.Cargo}</Text>
-            <Text>Nome: {posts['Nome:']}</Text>
-            <Text>Salário: {posts.Salario}</Text>
+            <View style={style.containerHeader}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', marginLeft: 5 }}>Situação dos Desvios</Text>
+                <Text style={{borderRadius:8, width:"90%", textAlign:'center', fontSize: 18, marginLeft: 5, backgroundColor:"#FF5733", marginTop:5, color:"#fff" }}>Em Atraso: {posts.atraso}</Text>
+                <Text style={{borderRadius:8, width:"90%", textAlign:'center', fontSize: 18, marginLeft: 5, backgroundColor:"#33FF57", marginTop:5 }}>Concluído: {posts.concluido}</Text>
+                <Text style={{borderRadius:8, width:"90%", textAlign:'center',color:"#fff", fontSize: 18, marginLeft: 5, backgroundColor:"blue", marginTop:5 }}>Em andamento: {posts.andamento}</Text>
+            </View>
+
+            <View style={style.containerChart}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Desvios</Text>
+                <View style={style.chart}>
+                    <ChartPizza atraso={posts.atraso} concluido={posts.concluido} andamento={posts.andamento}/>
+                </View>
+            </View>
         </View>
     )
 }
 const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f3f3f3"
+    },
+    containerHeader: {
+        marginTop: 12,
+        fontSize: 15,
+        margin: 14,
+        backgroundColor: "#fff",
+        padding: 5,
+        borderRadius: 8,
+        alignItems:'center'
+    },
+    containerChart: {
+        width: "92%",
+        backgroundColor: "#fff",
+        height: "30%",
+        padding: 4,
+        marginLeft: 15,
+        alignContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8
+    },
+    chart: {
 
+    }
 })
---
+++ src/pages/components/chartPizza.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+//import { PieChart,Legend } from 'react-native-svg-charts';
+//import { PieChart, Legend } from 'react-native-svg-charts';
+import { PieChart,  } from 'react-native-svg-charts';
+
+const ChartPizza  = ({ atraso, concluido, andamento }) => {
+    const data = [
+      {
+        key: 1,
+        value: atraso,
+        svg: { fill: '#FF5733' },
+        arc: { outerRadius: '100%', padAngle: 0.02, labels:40 },
+       
+      },
+      {
+        key: 2,
+        value: concluido,
+        svg: { fill: '#33FF57' },
+      },
+      {
+        key: 3,
+        value: andamento,
+        svg: { fill: 'blue' },
+      }
+    ];
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <PieChart style={{ height: 200, width: 130 }} data={data} labels ={20}/>
+      </View>
+    );
+  };
+  export default ChartPizza;
+  
